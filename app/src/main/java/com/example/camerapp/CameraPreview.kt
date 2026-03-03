4c4f3486e53025586ff50a6b6cbc2af14111242d
kien6996 <nvkien.192007@gmail.com>
2026-03-03 11:42:41 +0700

    Create CameraPreview.kt for camera preview and lens switching

--- app/src/main/java/com/example/camerapp/CameraPreview.kt
+++ app/src/main/java/com/example/camerapp/CameraPreview.kt
@@ -1,78 +1,114 @@
 package com.example.camerapp
 
 import android.content.Context
-import android.hardware.Camera
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import java.io.IOException
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CameraMetadata
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.TotalCaptureResult
+import android.media.Image
+import android.media.ImageReader
+import android.view.Surface
+import android.view.TextureView
+import android.view.View
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class CameraPreview(context: Context, private val camera: Camera) : SurfaceView(context), SurfaceHolder.Callback {
+class CameraPreview : AppCompatActivity() {
+    private lateinit var cameraManager: CameraManager
+    private lateinit var cameraDevice: CameraDevice
+    private lateinit var cameraCaptureSession: CameraCaptureSession
 
-    private val holder: SurfaceHolder = getHolder()
-    private var currentCameraId = 0
-    private var isRecording = false
+    private lateinit var captureRequestBuilder: CaptureRequest.Builder
+    private lateinit var textureView: TextureView
 
-    init {
-        holder.addCallback(this)
+    private var cameraId: String? = null
+    private var lensFacing: Int = CameraMetadata.LENS_FACING_BACK
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_preview)
+
+        textureView = findViewById(R.id.textureView)
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        // Enable the camera preview functionality on textureView
+        setupCameraPreview()
     }
 
-    override fun surfaceCreated(holder: SurfaceHolder) {
+    private fun setupCameraPreview() {
         try {
-            camera.setPreviewDisplay(holder)
-            camera.startPreview()
-        } catch (e: IOException) {
+            cameraId = chooseCamera()
+            if (cameraId != null) {
+                cameraManager.openCamera(cameraId!!, object : CameraDevice.StateCallback() {
+                    override fun onOpened(@NonNull cameraDevice: CameraDevice) {
+                        this@CameraPreview.cameraDevice = cameraDevice
+                        createCameraPreviewSession()
+                    }
+
+                    override fun onDisconnected(@NonNull cameraDevice: CameraDevice) {
+                        cameraDevice.close()
+                    }
+
+                    override fun onError(@NonNull cameraDevice: CameraDevice, error: Int) {
+                        cameraDevice.close()
+                    }
+                }, null)
+            }
+        } catch (e: CameraAccessException) {
             e.printStackTrace()
         }
     }
 
-    override fun surfaceDestroyed(holder: SurfaceHolder) {
-        camera.stopPreview()
-        camera.release()
-    }
-
-    override fun surfaceChanged(holder: SurfaceHolder, format: Int, w: Int, h: Int) {
-        if (holder.surface == null) {
-            return
-        }
-
-        try {
-            camera.stopPreview()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        try {
-            camera.setPreviewDisplay(holder)
-            camera.startPreview()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-
-    fun switchCamera(cameraId: Int) {
-        if (currentCameraId != cameraId && !isRecording) {
-            currentCameraId = cameraId
-            camera.stopPreview()
-            camera.release()
-            val newCamera = Camera.open(cameraId)
-            try {
-                newCamera.setPreviewDisplay(holder)
-                newCamera.startPreview()
-            } catch (e: IOException) {
-                e.printStackTrace()
+    private fun chooseCamera(): String? {
+        // Code to choose camera based on lensFacing
+        val cameraIdList = cameraManager.cameraIdList
+        for (id in cameraIdList) {
+            val characteristics = cameraManager.getCameraCharacteristics(id)
+            val facing = characteristics.get(CameraCharacteristics.LENS_FACING)
+            if (facing != null && facing == lensFacing) {
+                return id
             }
         }
+        return null
     }
 
-    fun startRecording() {
-        isRecording = true
+    private fun createCameraPreviewSession() {
+        try {
+            val surface = Surface(textureView.surfaceTexture)
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            captureRequestBuilder.addTarget(surface)
+
+            cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+                override fun onConfigured(@NonNull session: CameraCaptureSession) {
+                    cameraCaptureSession = session
+                    try {
+                        cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null)
+                    } catch (e: CameraAccessException) {
+                        e.printStackTrace()
+                    }
+                }
+
+                override fun onConfigureFailed(@NonNull session: CameraCaptureSession) {
+                }
+            }, null)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
     }
 
-    fun stopRecording() {
-        isRecording = false
-    }
-
-    fun isRecordingVideo(): Boolean {
-        return isRecording
+    fun switchLens() {
+        lensFacing = if (lensFacing == CameraMetadata.LENS_FACING_BACK) {
+            CameraMetadata.LENS_FACING_FRONT
+        } else {
+            CameraMetadata.LENS_FACING_BACK
+        }
+        // Close current camera and re-initialize
+        cameraDevice.close()
+        setupCameraPreview()
     }
 }
